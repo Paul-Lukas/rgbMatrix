c2612e118ab5378fad66cd3ff34c47b6a2fc0a3e
Paul-Lukas <paul.lukas.schaefer@gmail.com>
2021-12-10 13:42:42 +0100

    foo

--- Plugins/BootPlugin.py
+++ Plugins/BootPlugin.py
@@ -9,52 +9,57 @@ from Libraries import matrix
 
 
 class BootPlugin(Plugin):
-    def __init__(self):
-        pass
-        #super.__init__()
+    def __init__(self, app, matrix):
+        super().__init__(app, matrix)
 
 
+    def run(self):
+        omatrix = self.matrix
 
-        def run(self):
-            pixels = neopixel.NeoPixel(board.D18, 450, auto_write = False)
-            omatrix = matrix.NeoMatrix(15, 30, pixels)
+        omatrix.fill_all((0, 0, 0))
+        time.sleep(1)
+        # R
+        omatrix.fill_all((255, 0, 0))
+        time.sleep(1)
+        # G
+        omatrix.fill_all((0, 255, 0))
+        time.sleep(1)
+        # B
+        omatrix.fill_all((0, 0, 255))
+        time.sleep(1)
+        omatrix.fill_all((255, 255, 255))
+        time.sleep(1)
+        omatrix.fill_all((0, 0, 0))
+        time.sleep(1)
 
-            omatrix.fill_all((0, 0, 0))
-            time.sleep(1)
-            # R
-            omatrix.fill_all((255, 0, 0))
-            time.sleep(1)
-            # G
-            omatrix.fill_all((0, 255, 0))
-            time.sleep(1)
-            # B
-            omatrix.fill_all((0, 0, 255))
-            time.sleep(1)
-            omatrix.fill_all((255, 255, 255))
-            time.sleep(1)
-            omatrix.fill_all((0, 0, 0))
-            time.sleep(1)
+        omatrix[0, 0] = (0, 0, 255)
+        omatrix[14, 0] = (0, 255, 255)
+        omatrix[0, 29] = (0, 255, 255)
+        omatrix[14, 29] = (0, 255, 0)
 
-            omatrix[0, 0] = (0, 0, 255)
-            omatrix[14, 0] = (0, 255, 255)
-            omatrix[0, 29] = (0, 255, 255)
-            omatrix[14, 29] = (0, 255, 0)
+        omatrix.submit_all()
 
-            omatrix.submit_all()
+        time.sleep(2)
 
-            time.sleep(2)
+        for i in range(30):
+            for j in range(15):
+                if (i + j) % 2 == 0:
+                    omatrix[j, i] = (255, 0, 0)
+                else:
+                    omatrix[j, i] = (0, 0, 255)
 
-            for i in range(30):
-                for j in range(15):
-                    if (i + j) % 2 == 0:
-                        omatrix[j, i] = (255, 0, 0)
-                    else:
-                        omatrix[j, i] = (0, 0, 255)
+                time.sleep(0.25)
+                omatrix.submit_all()
 
-                    time.sleep(0.25)
-                    omatrix.submit_all()
+        omatrix.fill_all((0, 0, 0))
+        omatrix.submit_all()
 
-            omatrix.fill_all((0, 0, 0))
-            omatrix.submit_all()
+        for i in range(10):
+            if i % 2 == 0:
+                omatrix.fill_all((0, 255, 255))
+                omatrix.submit_all()
+            else:
+                omatrix.fill_all((255, 0, 255))
+                omatrix.submit_all()
 
-            print("Fertig")
+        print("Fertig")
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from Application.BaseApplication import BaseApplication
 
+import board
+import neopixel
+
 if __name__ == '__main__':
-    app = BaseApplication()
+    pixels = neopixel.NeoPixel(board.D18, 450, auto_write = False)
+    omatrix = matrix.NeoMatrix(15, 30, pixels)
+
+    app = BaseApplication(omatrix)
     app.run()
