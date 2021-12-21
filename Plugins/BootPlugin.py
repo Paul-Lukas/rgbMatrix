0e316d99b419fad2995a5b4a5627e80c83280a39
Paul-Lukas <paul.lukas.schaefer@gmail.com>
2021-12-21 21:53:14 +0100

    bootPlugin auskommentieren

--- Plugins/BootPlugin.py
+++ Plugins/BootPlugin.py
@@ -2,64 +2,72 @@ from Plugins.Plugin import Plugin
 
 import time
 
-import board
-import neopixel
-
-from Libraries import matrix
-
 
 class BootPlugin(Plugin):
     def __init__(self, app, matrix):
         super().__init__(app, matrix)
 
 
+    #Run wird beim start ausgeführt und muss den Code enthalten, der im endeffekt ausgeführt werden soll
     def run(self):
-        omatrix = self.matrix
 
-        omatrix.fill_all((0, 0, 0))
+        #self.matrix wird beim ausführen des super constructors gesetzt
+        #self.matrix ist eine Instanz der matrix Klasse zu finden in Libraries/matrix.py
+        #Eine beschreibung aller Methoden ist btw auch in Libraries/matrix.py zu findes, nur so btw
+        #fill_all setzt alles auf eine Farbe, die Farbe muss als tupel mit 3 werten angegeben werden. RGB halt
+        self.matrix.fill_all((0, 0, 0))
         time.sleep(1)
         # R
-        omatrix.fill_all((255, 0, 0))
+        self.matrix.fill_all((255, 0, 0))
         time.sleep(1)
         # G
-        omatrix.fill_all((0, 255, 0))
+        self.matrix.fill_all((0, 255, 0))
         time.sleep(1)
         # B
-        omatrix.fill_all((0, 0, 255))
-        time.sleep(1)
-        omatrix.fill_all((255, 255, 255))
-        time.sleep(1)
-        omatrix.fill_all((0, 0, 0))
+        self.matrix.fill_all((0, 0, 255))
         time.sleep(1)
 
-        omatrix[0, 0] = (0, 0, 255)
-        omatrix[14, 0] = (0, 255, 255)
-        omatrix[0, 29] = (0, 255, 255)
-        omatrix[14, 29] = (0, 255, 0)
+        #Weiß
+        self.matrix.fill_all((255, 255, 255))
+        time.sleep(1)
+        #Alles Löschen / Schwarz
+        self.matrix.fill_all((0, 0, 0))
+        time.sleep(1)
 
-        omatrix.submit_all()
+        #self.matrix[x, y] = (r, g, b) Setzt den Pixel mit der koordinate x, y auf den wert aus der Klammer
+        #Die Änderung ist noch nicht zu sehen, aber vorgemerkt, sie wird mit submit_all an das Bord übertragen
+        self.matrix[0, 0] = (0, 0, 255)
+        self.matrix[14, 0] = (0, 255, 255)
+        self.matrix[0, 29] = (0, 255, 255)
+        self.matrix[14, 29] = (0, 255, 0)
+
+        self.matrix.submit_all()
 
         time.sleep(2)
+
+        #Wenn du eine Variable VOR self.matrix[x, y] stehen hast, dann nimmt diese Variable den tupel wert an
+        #Achtung, es werden die gemerkten werte zurück gegeben, sprich das bord kann anders sein, falls noch nicht submit_all aufgerufen wurde
+        farbwert = self.matrix[0, 0]
+
+
+        #Die Letzte Möglickeit, für die ich aber gerade leider keine gescheite Anwendung hab ist es, dass ganze Array zu Tauschen
+        testMatrix = [[(0, 0, 0) for j in range(15)] for i in range(30)]
+
+        self.matrix.set_matrix(testMatrix)
+        #Auch danach submitt_all machen
+        self.matrix.submit_all()
+
 
         for i in range(30):
             for j in range(15):
                 if (i + j) % 2 == 0:
-                    omatrix[j, i] = (255, 0, 0)
+                    self.matrix[j, i] = (255, 0, 0)
                 else:
-                    omatrix[j, i] = (0, 0, 255)
+                    self.matrix[j, i] = (0, 0, 255)
 
                 time.sleep(0.25)
-                omatrix.submit_all()
+                self.matrix.submit_all()
 
-        omatrix.fill_all((0, 0, 0))
-        omatrix.submit_all()
-
-        for i in range(10):
-            if i % 2 == 0:
-                omatrix.fill_all((0, 255, 255))
-                omatrix.submit_all()
-            else:
-                omatrix.fill_all((255, 0, 255))
-                omatrix.submit_all()
-
+        self.matrix.fill_all((0, 0, 0))
+        self.matrix.submit_all()
         print("Fertig")
