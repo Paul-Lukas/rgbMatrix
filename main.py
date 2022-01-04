d480500b2556068e06790a38536cec95f9077142
Paul-Lukas <68750116+Paul-Lukas@users.noreply.github.com>
2022-01-04 21:58:10 +0100

    use gui

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from Application.BaseApplication import BaseApplication
-from Libraries import matrix
-
-import board
-import neopixel
+from Libraries import matrixGui
 
 if __name__ == '__main__':
-    pixels = neopixel.NeoPixel(board.D18, 450, auto_write = False)
-    omatrix = matrix.NeoMatrix(15, 30, pixels)
+    omatrix = matrixGui.NeoMatrixGui(15, 30, pixels)
 
     app = BaseApplication(omatrix)
     app.run()
