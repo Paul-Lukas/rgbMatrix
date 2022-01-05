fc1f3f7fceb237318993df3fc19fa42a47fa24eb
Paul-Lukas <68750116+Paul-Lukas@users.noreply.github.com>
2022-01-05 08:05:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Application.BaseApplication import BaseApplication
 from Libraries import matrixGui
 
 if __name__ == '__main__':
-    omatrix = matrixGui.NeoMatrixGui(15, 30, pixels)
+    omatrix = matrixGui.NeoMatrixGui(15, 30)
 
     app = BaseApplication(omatrix)
     app.run()
