37ff37bcee3a38dcfd2cff15991e55a149d11229
Paul-Lukas <68750116+Paul-Lukas@users.noreply.github.com>
2022-02-04 13:24:14 +0100

    Update matrix.py

--- Libraries/matrix.py
+++ Libraries/matrix.py
@@ -1,4 +1,5 @@
 import neopixel
+from Libraries.utils import Utils
 
 
 class NeoMatrix:
@@ -7,19 +8,21 @@ class NeoMatrix:
     The Application must be horizontal
     """
     matrix = []
+    omatrix = []
     width = 0
     height = 0
 
     __pixels = object
-
 
     def __init__(self, width: int, height: int, pixels: neopixel.NeoPixel):
         self.width = width
         self.height = height
 
         self.__pixels = pixels
+        self.utils = Utils()
 
         self.matrix = [[(0, 0, 0) for j in range(height)] for i in range(width)]
+        self.omatrix = [[(0, 0, 0) for j in range(height)] for i in range(width)]
 
 
     def __getitem__(self, item):
@@ -27,23 +30,23 @@ class NeoMatrix:
             raise ValueError('Index needs two values example: [1, 2]')
         return self.matrix[item[0]][item[1]]
 
-
     def __setitem__(self, key, value):
         if len(key) != 2:
             raise ValueError('Index needs two values example: [1, 2]')
         self.matrix[key[0]][key[1]] = value
 
-
     def submit_all(self):
         """
         Writes all the changes to tne Neopixel String
         """
-        i = 0
-        for pixel in self.__convert_matrix(self.matrix):
-            self.__pixels[i] = pixel
-            i += 1
-        self.__pixels.write()
+        changes = self.utils.getChangedIndices(self.omatrix, self.matrix)
 
+        self.omatrix = [row[:] for row in self.matrix]
+
+        for i in range(len(changes)):
+            pixelChange = (self.utils.getNumForCords(changes[i][0], changes[i][1], self.height))
+            self.__pixels[pixelChange] = self.matrix[changes[i][0]][changes[i][1]]
+        self.__pixels.write()
 
     def fill_all(self, color: tuple):
         """
@@ -55,7 +58,6 @@ class NeoMatrix:
                 self.matrix[i][j] = color
         self.__pixels.fill(color)
         self.__pixels.write()
-
 
     def set_matrix(self, matrix):
         """
@@ -71,59 +73,3 @@ class NeoMatrix:
             else:
                 self.matrix = matrix
                 return True
-
-
-    def __convert_matrix(self, matrix) -> list:
-        """
-        Converts a specific Application to an Array
-        Example: input=  [
-                            [1, 2, 3, 4, 5],
-                            [10, 9, 8, 7, 6],
-                            [11, 12, 13, 14, 15]
-                         ]
-        :param matrix: Application(two dimensional array) uneven columns flipped
-        :return: one dimensional Array
-        """
-        index = []
-        for i in range(len(matrix)):
-            if i % 2 == 0:
-                index.append(matrix[i])
-            else:
-                index.append(self.__flip_array(matrix[i]))
-
-        return self.__flatten_array(index)
-
-
-    def __flip_array(self, array: list) -> list:
-        """
-        Flips an array
-        Example: input=  [1, 2, 3, 4, 5]
-                 Output= [5, 4, 3, 2, 1]
-        :param array: Array to flip
-        :return: Flipped Array
-        """
-        flipped = []
-        for i in range(len(array)):
-            index = i * (-1) - 1
-            flipped.append(array[index])
-        return flipped
-
-
-    def __flatten_array(self, array: list) -> list:
-        """
-        Flattens two dimensional array to one dimensional
-        Example: input=  [
-                            [1, 2, 3, 4, 5],
-                            [6, 7, 8, 9, 10]
-                         ]
-
-                 Output= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        :param array: two dimensional array
-        :return: one dimensional array
-        """
-        flat = []
-
-        for i in array:
-            for j in i:
-                flat.append(j)
-        return flat
